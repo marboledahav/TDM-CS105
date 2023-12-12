26dfe177549d2c750f693615d807067fdaef7956
marboledahav <147944675+marboledahav@users.noreply.github.com>
2023-12-12 03:29:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,24 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 @app.route('/recommendations', methods=['POST'])
 def recommendations():
-    user_preferences = collect_user_preferences()
-    destinations = initialize_destinations()
-    best_destinations = find_best_destination(user_preferences, destinations)
-
-    top_destination = best_destinations[0][0]
-    second_destination = best_destinations[1][0]
-    third_destination = best_destinations[2][0]
-
-    return render_template(
-        'recommendations.html',
-        top_destination=top_destination,
-        second_destination=second_destination,
-        third_destination=third_destination
-    )
-
+    if request.method == 'POST':
+        user_preferences = {
+            'budget': float(request.form['budget']),
+            'activities': request.form['activities'].split(','),
+            'climate_preference': request.form['climate_preference']
+        }
+        
+        destinations = initialize_destinations()
+        best_destinations = find_best_destination(user_preferences, destinations)
+        
+        return render_template('recommendations.html', best_destinations=best_destinations)
 
 if __name__ == '__main__':
     app.run(debug=True)
